9a05c964b94bf02b9fdee0ee9e26ee277f543358
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-01-25 21:47:48 +0100

    add dayWeather, hightligh and days weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import "./App.css";
+import { Layout } from "./layout/Layout";
+import theme from "./schemas/theme";
 import GlobalStyled from "./styles/global";
 
 const App = () => (
-  <ThemeProvider theme={}>
+  <ThemeProvider theme={theme}>
     <GlobalStyled />
+    <Layout></Layout>
   </ThemeProvider>
 );
 
