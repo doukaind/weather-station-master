cf092c8b4f9064c776e5fb3b682d91ce009a26bd
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-01-29 21:25:54 +0100

    add global css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyled = createGlobalStyle`
-* {
-        box-sizing: border-box
-    }  
-    
+
  html {
-     margin: 0;
+     box-sizing: border-box;
      -ms-overflow-style: scrollbar;
       font-family: Karla,serif;
+ }
+
+ ul {
+     padding: 0;
+     margin: 0;
+ }
+
+ li {
+     list-style: none;
  }
 
 `;
