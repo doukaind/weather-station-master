ce6c8cadae331e2300ffc7b68dac2786eccc9593
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-02-26 13:48:22 +0100

    add navigation

--- src/common/aside/SidebarWrapper.ts
+++ src/common/aside/SidebarWrapper.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const SidebarWrapper = styled.aside`
-  background-color: ${(props) => props.theme.color.blueBase};
+  /* background-color: ${(props) => props.theme.color.blueBase}; */
   display: grid;
   align-items: center;
   justify-items: center;
---
+++ src/common/navigation/SearchNavWrapper.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SearchNavWrapper = styled.div`
+  position: fixed;
+  /* overflow-y: auto; */
+`;
