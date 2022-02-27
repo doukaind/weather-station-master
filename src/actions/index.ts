9f56f13e6500f43a98e0cdefe6abc450c90b0339
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-02-27 22:03:27 +0100

    add sidebar elements

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,5 @@ import { SWITCH_DEGREE_TYPE } from "../actionTypes/degrees";
 export const switchDegreeType = () => ({
   type: SWITCH_DEGREE_TYPE,
 });
+
+export const fetchLocation = async (dispatch: any) => {};
